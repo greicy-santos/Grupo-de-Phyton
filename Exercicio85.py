03b61c3d57fbbd3e92657551e15fdf6cdc94b901
greicy-santos <greicy.santos@fatec.sp.gov.br>
2017-08-15 23:35:27 -0300

    Update Exercicio85.py

--- Exercicio85.py
+++ Exercicio85.py
@@ -1,5 +1,3 @@
-85:Faça como o 84 porem adicione elifs depois do if , o código vai parar na primeira condição caso ela passe, explique por que isso acontece .
-
 pizza = ['mussarela','calabreza','bacon']
 
 if 'mussarela' in pizza:
